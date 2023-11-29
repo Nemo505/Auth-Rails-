bc87bd762db0c15f6acae7fd75a8d44e1c071490
nemo <dahliahalesia8@gmail.com>
2023-11-29 15:22:31 +0630

    ui

--- app/controllers/viber_webhook_controller.rb
+++ app/controllers/viber_webhook_controller.rb
@@ -1,43 +1,31 @@
 class ViberWebhookController < ApplicationController
-    skip_before_action :verify_authenticity_token
-  
-    def receive
-      # Handle incoming Viber messages here
-      # Parse incoming JSON payload from Viber
-      data = JSON.parse(request.body.read)
-  
-      # Implement logic to handle different events (message, join, leave, etc.)
-      # Example: reply to a message
-      if data['event'] == 'message'
-        sender_id = data['sender']['id']
-        message_text = data['message']['text']
-  
-        # Implement your response logic here
-        response_text = "You said: #{message_text}"
-  
-        # Send the response back to the user
-        send_message(sender_id, response_text)
-      end
-  
-      head :ok
-    end
-  
-    private
-  
-    def set_viber_service
-        @viber_service = ViberService.new('520017e61327e172-8ce91a4a8b8c03b1-60aa8e241d9bf01c')
-      end
-    
-      def send_message(receiver_id, text)
-        # Use the Viber API to send a message
-        result = @viber_service.send_message(receiver_id, text)
-    
-        # Handle the result if needed
-        if result.key?('error')
-          Rails.logger.error("Viber API error: #{result['error']}")
-        else
-          Rails.logger.info('Message sent successfully!')
-        end
-    end
+  skip_before_action :verify_authenticity_token
+
+  def new
+
   end
+
+  def receive
+    user_message = params[:message]
+
+    # Call the send_message method with the user's message
+    bot_response = send_message(user_message)
+
+    # Render the response as JSON
+    render json: { bot_response: bot_response }
+  end
+
+  private
+
+  def send_message(user_message)
+    # Handle sending messages and receiving responses here
+    # You can use user_message to get the user's message
+
+    # Simulate a response from the bot for testing
+    bot_response = "I received: #{user_message}"
+
+    bot_response
+  end
+end
+
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   # get "register", to:"auth#register"
 
-  root 'chat_bot#index'
+  root 'viber_webhook#new'
   resource :registration
   # config/routes.rb
   resources :sessions, only: [:new, :create, :destroy]
 
-  
   post '/viber_webhook/receive', to: 'viber_webhook#receive'
-  get '/chatbot', to: 'chat_bot#index'
-  post '/send_message', to: 'chatbot#send_message'
 end
