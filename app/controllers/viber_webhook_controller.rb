9047d6d7e3e9a887b57388d8ba7f735ef3981fb3
nemo505 <dahliahalesia8@gmail.com>
2023-12-06 22:57:38 +0630

    ttsModel

--- app/controllers/viber_webhook_controller.rb
+++ app/controllers/viber_webhook_controller.rb
@@ -8,6 +8,7 @@ class ViberWebhookController < ApplicationController
   def receive
     user_message = params[:message]
     user_image = params[:image]
+    user_audio = params[:audio]
 
     if user_image.present? && user_image == "generate"
       # Call DALL-E API for image generation
@@ -15,6 +16,12 @@ class ViberWebhookController < ApplicationController
 
       # Render the generated image URL as JSON
       render json: { generated_image: generated_image }
+    elsif user_audio.present? && user_audio == "speak"
+      # Call a method to process the audio message
+      processed_audio = process_audio(user_message)
+
+      # Render the processed audio data as JSON
+      render json: { processed_audio: processed_audio }
     else
       # Call the send_message method with the user's message
       bot_response = send_message(user_message)
@@ -22,15 +29,13 @@ class ViberWebhookController < ApplicationController
       # Render the response as JSON
       render json: { bot_response: bot_response }
     end
-
   end
 
   private
 
-  # openai_client = OpenAI::Client.new(api_key: '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' , default_engine: "ada")
   def send_message(user_message)
-    # Use your OpenAI API key here
-    client = OpenAI::Client.new(access_token: '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')
+    # Use OpenAI API key here
+    client = OpenAI::Client.new(access_token: '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')
   
     response = client.chat(
       parameters: {
@@ -45,7 +50,7 @@ class ViberWebhookController < ApplicationController
 
   def generate_image(prompt)
 
-    client = OpenAI::Client.new(access_token: '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')
+    client = OpenAI::Client.new(access_token: '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')
 
     response = client.images.generate(parameters: { prompt: prompt, size: "256x256" })
 
@@ -54,5 +59,32 @@ class ViberWebhookController < ApplicationController
 
     generated_image
   end
+
+  def process_audio(user_audio)
+    client = OpenAI::Client.new(access_token: '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')
+    response = client.audio.speech(
+      parameters: {
+        model: "tts-1",
+        input: user_audio,
+        voice: "alloy"
+      }
+    )
+
+    # Define the file path
+    audio = Rails.root.join('public', 'demo.mp3')
+
+    # # Write the audio content to the file
+    File.binwrite(audio, response)
+
+    # Get the relative path from the 'public' directory
+    relative_path = Pathname.new('demo.mp3')
+
+    # Generate the localhost URL
+    processed_audio = "http://localhost:3000/#{relative_path}"
+
+    # Return the file path
+    processed_audio
+  end
+
 
 end
