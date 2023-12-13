88c91437c204d81b9505a93292cd97ec91a6f0f6
nemo505 <dahliahalesia8@gmail.com>
2023-12-13 09:54:35 +0630

    embedding

--- app/controllers/viber_webhook_controller.rb
+++ app/controllers/viber_webhook_controller.rb
@@ -1,4 +1,6 @@
 require 'openai'
+require 'tempfile'
+
 class ViberWebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
@@ -9,6 +11,7 @@ class ViberWebhookController < ApplicationController
     user_message = params[:message]
     user_image = params[:image]
     user_audio = params[:audio]
+    user_embedding = params[:embedding]
 
     if user_image.present? && user_image == "generate"
       # Call DALL-E API for image generation
@@ -16,12 +19,25 @@ class ViberWebhookController < ApplicationController
 
       # Render the generated image URL as JSON
       render json: { generated_image: generated_image }
+    elsif user_embedding.present? && user_embedding == "embedding"
+      # Call a method to process the audio message
+      embedding_response = send_embedding(user_embedding)
+
+      render json: { embedding_response: embedding_response }
     elsif user_audio.present? && user_audio == "speak"
       # Call a method to process the audio message
       processed_audio = process_audio(user_message)
 
-      # Render the processed audio data as JSON
       render json: { processed_audio: processed_audio }
+
+    elsif params[:file].present?
+      # Access the file data
+      file_data = params[:file].tempfile.read
+
+      processed_file = process_file(file_data)
+
+      render json: { processed_file: processed_file }
+
     else
       # Call the send_message method with the user's message
       bot_response = send_message(user_message)
@@ -86,5 +102,48 @@ class ViberWebhookController < ApplicationController
     processed_audio
   end
 
+  def process_file(audio_data)
+      client = OpenAI::Client.new(access_token: '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')
+
+      # Create a temporary file and write the audio data to it
+      temp_file = Tempfile.new(['audio', '.wav'])
+      temp_file.binmode
+      temp_file.write(audio_data)
+      temp_file.rewind
+
+      begin
+        response = client.audio.transcribe(
+          parameters: {
+            model: "whisper-1",
+            file: File.open(temp_file.path, "rb"),
+          }
+        )
+
+        # Extract the transcribed text from the response
+        processed_file = response.dig("text")
+        processed_file
+      ensure
+        # Close and delete the temporary file
+        temp_file.close
+        temp_file.unlink
+      end
+  end
+
+  def send_embedding(user_embedding)
+    # Use OpenAI API key here
+    client = OpenAI::Client.new(access_token: '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')
+  
+   
+    response = client.embeddings(
+      parameters: {
+          model: "text-embedding-ada-002",
+          input: "The food was delicious and the waiter..."
+      }
+    )
+
+    # Extract the generated text from the response
+    embedding_response = response.dig("data", 0, "embedding")
+    embedding_response
+  end
 
 end
